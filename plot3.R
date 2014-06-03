1806c9a7a97adc0f13a35059aba53b5cfc9e0339
drifap <drifapalin@gmail.com>
2014-06-03 15:22:03 +0000

    updated code for plot 3

--- plot3.R
+++ plot3.R
@@ -2,14 +2,24 @@
 
 source(file="load_data.R")
 
+# create function for plot 3 with legend
 plot3 <- function(){
-        #plot3
-        png("plot3.png")
-        plot(sub$new_Date,sub$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="");
-        lines(sub$new_Date,sub$Sub_metering_2, col="red",lwd=2);
-        lines(sub$new_Date,sub$Sub_metering_3, col="blue",lwd=2);
-        legend("topright",pch=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-        dev.off()
+        png("plot3.png")                                     # open grapichs device
+        plot(sub$new_Date,sub$Sub_metering_1,                # set initial plot 
+             type="l",                                       # line type
+             ylab="Energy sub metering",                     # label Y
+             xlab="");                                       # remove label X
+        lines(sub$new_Date,sub$Sub_metering_2,               # add lines to plot
+              col="red",                                     # colour for line 2
+              lwd=2);                                        # determine width of line
+        lines(sub$new_Date,sub$Sub_metering_3,               # add lines to plot 
+              col="blue",                                    # colour for line 2
+              lwd=2);                                        # determine width of line
+        legend("topright",                                   # add legend to plot
+               pch=1,                                        # need to fix - lines not dots
+               col=c("black","red","blue"),                  # colour in legend
+               legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+        dev.off()                                            # graphics off
 }
 
 plot3()
